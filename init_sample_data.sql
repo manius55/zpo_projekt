a85050e596fe7eecf20fdf98c1439c04474690cc
manius55 <m.biedrzycki@o2.pl>
2025-06-03 21:47:58 +0200

    Inicjacja przykladowych danych => pierwsze rekordy napojow alkoholowych

---
+++ init_sample_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Alcohols (Name, Percentage, Type, Description) VALUES ('Lech', 5.5, 'Piwo', 'Aged whiskey');
+INSERT INTO Alcohols (Name, Percentage, Type, Description) VALUES ('Warka', 5.0, 'Piwo', NULL);
+INSERT INTO Alcohols (Name, Percentage, Type, Description) VALUES ('Carlo Rossi', 12.5, 'Wino', 'tanie bylo');
