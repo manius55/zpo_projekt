3e3c5db8a840a94a34259832671f22402a1bf0b7
manius55 <m.biedrzycki@o2.pl>
2025-06-03 22:00:53 +0200

    Inicjacja przykladowych danych => typ jako int

--- init_sample_data.sql
+++ init_sample_data.sql
@@ -1,3 +1,3 @@
-INSERT INTO Alcohols (Name, Percentage, Type, Description) VALUES ('Lech', 5.5, 'Piwo', 'Aged whiskey');
-INSERT INTO Alcohols (Name, Percentage, Type, Description) VALUES ('Warka', 5.0, 'Piwo', NULL);
-INSERT INTO Alcohols (Name, Percentage, Type, Description) VALUES ('Carlo Rossi', 12.5, 'Wino', 'tanie bylo');
+INSERT INTO Alcohols (Name, Percentage, Type, Description) VALUES ('Lech', 5.5, 1, 'Aged whiskey');
+INSERT INTO Alcohols (Name, Percentage, Type, Description) VALUES ('Warka', 5.0, 1, NULL);
+INSERT INTO Alcohols (Name, Percentage, Type, Description) VALUES ('Carlo Rossi', 12.5, 2, 'tanie bylo');
